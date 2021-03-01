032c958b0249c1659cf481524e1db0a8294993fa
Skúli Þór Árnason <skulithor94@gmail.com>
2021-03-01 21:46:51 +0000

    Blaaa

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,130 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </header> */}
+      {/* <div className="grid grid-cols-3 gap-4"> */}
+      {/* <figure className="bg-gray-100 rounded-xl p-8">
+        <img className="w-32 h-32 rounded-full mx-auto" src={logo} alt="" width="384" height="512"/>
+        <div className="pt-6 space-y-4">
+          <blockquote>
+            <p className="text-lg font-semibold">
+              “Tailwind CSS is the only framework that I've seen scale
+              on large teams. It’s easy to customize, adapts to any design,
+              and the build size is tiny.”
+            </p>
+          </blockquote>
+          <figcaption className="font-medium">
+            <div>
+              Sarah Dayan
+            </div>
+            <div>
+              Staff Engineer, Algolia
+            </div>
+          </figcaption>
+        </div>
+      </figure>
+      <figure className="bg-gray-100 rounded-xl p-8">
+        <img className="w-32 h-32 rounded-full mx-auto" src={logo} alt="" width="384" height="512"/>
+        <div className="pt-6 space-y-4">
+          <blockquote>
+            <p className="text-lg font-semibold">
+              “Tailwind CSS is the only framework that I've seen scale
+              on large teams. It’s easy to customize, adapts to any design,
+              and the build size is tiny.”
+            </p>
+          </blockquote>
+          <figcaption className="font-medium">
+            <div>
+              Sarah Dayan
+            </div>
+            <div>
+              Staff Engineer, Algolia
+            </div>
+          </figcaption>
+        </div>
+      </figure>
+      <figure className="bg-gray-100 rounded-xl p-8">
+
+        <img className="w-32 h-32 rounded-full mx-auto" src={logo} alt="" width="384" height="512"/>
+        <div className="pt-6 space-y-4">
+          <blockquote>
+            <p className="text-lg font-semibold">
+              “Tailwind CSS is the only framework that I've seen scale
+              on large teams. It’s easy to customize, adapts to any design,
+              and the build size is tiny.”
+            </p>
+          </blockquote>
+          <figcaption className="font-medium">
+            <div>
+              Sarah Dayan
+            </div>
+            <div>
+              Staff Engineer, Algolia
+            </div>
+          </figcaption>
+        </div>
+      </figure> */}
+      <div className="min-h-screen  py-6 flex flex-col justify-center sm:py-12">
+        <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+          <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+          <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+            <div className="max-w-md mx-auto">
+              <div>
+                <img src={logo} className="h-7 sm:h-8" />
+              </div>
+              <div className="divide-y divide-gray-200">
+                <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+                  <p>An advanced online playground for Tailwind CSS, including support for things like:</p>
+                  <ul className="list-disc space-y-2">
+                    <li className="flex items-start">
+                      <span className="h-6 flex items-center sm:h-7">
+                        <svg className="flex-shrink-0 h-5 w-5 text-cyan-500" viewBox="0 0 20 20" fill="currentColor">
+                          <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
+                        </svg>
+                      </span>
+                      <p className="ml-2">
+                        Customizing your
+                  <code className="text-sm font-bold text-gray-900">tailwind.config.js</code> file
+                </p>
+                    </li>
+                    <li className="flex items-start">
+                      <span className="h-6 flex items-center sm:h-7">
+                        <svg className="flex-shrink-0 h-5 w-5 text-cyan-500" viewBox="0 0 20 20" fill="currentColor">
+                          <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
+                        </svg>
+                      </span>
+                      <p className="ml-2">
+                        Extracting classes with
+                  <code className="text-sm font-bold text-gray-900">@apply</code>
+                      </p>
+                    </li>
+                    <li className="flex items-start">
+                      <span className="h-6 flex items-center sm:h-7">
+                        <svg className="flex-shrink-0 h-5 w-5 text-cyan-500" viewBox="0 0 20 20" fill="currentColor">
+                          <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
+                        </svg>
+                      </span>
+                      <p className="ml-2">Code completion with instant preview</p>
+                    </li>
+                  </ul>
+                  <p>Perfect for learning how the framework works, prototyping a new idea, or creating a demo to share online.</p>
+                </div>
+                <div className="pt-6 text-base leading-6 font-bold sm:text-lg sm:leading-7">
+                  <p>Want to dig deeper into Tailwind?</p>
+                  <p>
+                    <a href="https://tailwindcss.com/docs" className="text-cyan-600 hover:text-cyan-700"> Read the docs &rarr; </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
+    // </div>
   );
 }
 
